873febe6f785139867b544aef6f6e96f82ee426a
Saleha2415 <88474763+Saleha2415@users.noreply.github.com>
2021-08-05 19:29:17 +0530

    Create Q-(9).js

---
+++ Q-(9).js
@@ -0,0 +1,23 @@
+
+/*
+How to access and print greet function from the above object code?
+*/
+
+
+const gimli =
+{ name: "Gimli",
+race: "dwarf",
+weapon: "axe",
+greet: function() 
+{ return `Hi, my name is ${this.name}!`; 
+},
+};
+console.log(gimli.greet());
+
+
+/*
+Output
+
+Hi, my name is Gimli!
+
+*/
