378225731309821e06ca75e2b6ea138a0611b5b8
Saleha2415 <88474763+Saleha2415@users.noreply.github.com>
2021-08-05 11:37:55 +0530

    Create Q-(1).js

---
+++ Q-(1).js
@@ -0,0 +1,9 @@
+//How to fetch the millisecond according to universal time from a given Date object?//
+
+var dateToday = new Date('October 15, 1996 05:35:32:77 GMT+11:00');
+console.log (dateToday.getUTCMilliseconds());
+
+/*
+Output
+77
+*/
